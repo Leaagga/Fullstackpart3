713a211656bc04673936b2bad36cd7cceddd84aa
Leaagga <135800909+Leaagga@users.noreply.github.com>
2023-10-02 22:15:53 +0800

    3a 3.2

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ let persons=[
 app.get('/api/persons',(request,response)=>{
     response.json(persons)
 })
+app.get('/info',(resquest,response)=>{
+    response.send(`<div><p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p></div>`)
+})
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
