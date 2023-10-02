2c82aef1558a06a81afc879581459899e869660b
Leaagga <135800909+Leaagga@users.noreply.github.com>
2023-10-02 22:34:14 +0800

    3a 3.4

--- index.js
+++ index.js
@@ -37,8 +37,12 @@ app.get('/api/persons/:id',(request,response)=>{
         response.json(person)
     }else{
         response.status(404).end()
-    }
-    
+    } 
+})
+app.delete('/api/persons/:id',(request,response)=>{
+    const id=Number(request.params.id)
+    persons=persons.filter(p=>p.id!==id)
+    response.status(204).end()
     
 })
 const PORT = 3001
